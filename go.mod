396c81b67b344a4774032d1ac6b226ccbc00d059
Felix <ofelix@pm.me>
2021-06-01 19:05:20 +0200

    Weights and pretty bars

--- go.mod
+++ go.mod
@@ -2,12 +2,12 @@ module github.com/felkr/roamer
 
 go 1.16
 
-// require github.com/hashicorp/nomad latest // indirect
-
 require (
+	github.com/fatih/color v1.12.0
 	github.com/hashicorp/hcl v1.0.1-vault-2 // indirect
 	github.com/hashicorp/hcl/v2 v2.9.2-0.20210407182552-eb14f8319bdc
 	github.com/hashicorp/nomad v1.1.0
-	github.com/hashicorp/nomad/api v0.0.0-20210527173017-41a43a98dc82 // indirect
+	github.com/hashicorp/nomad/api v0.0.0-20210527173017-41a43a98dc82
 	github.com/urfave/cli v1.22.5
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/hashicorp/hcl/v2/hclsimple"
 	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/nomad/jobspec"
@@ -14,13 +15,34 @@ import (
 
 type Config struct {
 	Infrastructure InfrastructureConfig `hcl:"infrastructure,block"`
+	Groups         []GroupConfig        `hcl:"group,block"`
 }
 
 type InfrastructureConfig struct {
-	Memory int `hcl:"memory"`
-	CPU    int `hcl:"cpu"`
+	Memory       int `hcl:"memory"`
+	CPU          int `hcl:"cpu"`
+	SafetyMargin int `hcl:"safety_margin,optional"`
+}
+type GroupConfig struct {
+	Name   string `hcl:"name,label"`
+	Weight int    `hcl:"weight"`
 }
 
+func distributeEvenly(value int, across int, config Config) int {
+	return (value / 100 * (100 - config.Infrastructure.SafetyMargin)) / across
+}
+func drawUnitBar(part int, full int, unit string, label string) {
+	const width = 25
+	bar := make([]rune, width)
+	fmt.Printf("%s\t%d/%d %s\t", label, part, full, unit)
+	for i := 0; i < width; i++ {
+		bar[i] = '\u25a1'
+	}
+	for i := 0; i < width*part/full; i++ {
+		bar[i] = '\u25a0'
+	}
+	fmt.Printf("%s %.2f%%\n", string(bar), float32(part)/float32(full)*100.0)
+}
 func main() {
 	jobspec.ParseFile("config.hcl")
 
@@ -34,36 +56,134 @@ func main() {
 		if c.NArg() < 2 {
 			cli.ShowAppHelpAndExit(c, 1)
 		}
-		job_path := c.Args().Get(1)
-		config_path := c.Args().Get(0)
+		jobPath := c.Args().Get(1)
+		configPath := c.Args().Get(0)
 
-		job_file, err := os.Open(job_path)
+		jobFile, err := os.Open(jobPath)
 
 		if os.IsNotExist(err) {
-			return cli.NewExitError(job_path+": No such file or directory", 1)
+			return cli.NewExitError(jobPath+": No such file or directory", 1)
 		}
 
-		job, err := jobspec2.Parse(job_path, job_file)
+		job, err := jobspec2.Parse(jobPath, jobFile)
 		if err != nil {
 			log.Fatal(err)
 		}
 		var config Config
-		err = hclsimple.DecodeFile(config_path, nil, &config)
+		err = hclsimple.DecodeFile(configPath, nil, &config)
 		if err != nil {
 			log.Fatalf("Failed to load configuration: %s", err)
 		}
-		fmt.Printf("CPU per group %d\n", config.Infrastructure.CPU/len(job.TaskGroups))
-		fmt.Printf("Memory per group %d\n", config.Infrastructure.Memory/len(job.TaskGroups))
-		fmt.Printf("%s\n", job)
+		if config.Infrastructure.SafetyMargin == 0 {
+			config.Infrastructure.SafetyMargin = 3
+		}
+		absoluteTasks := 0
+		for _, group := range job.TaskGroups {
+			tasksInGroup := len(group.Tasks)
 
-		client_config := api.DefaultConfig()
-		client_config.Address = "http://localhost:4646"
-		client, err := api.NewClient(client_config)
+			if group.Count != nil {
+				tasksInGroup *= *group.Count
+			}
+			absoluteTasks += tasksInGroup
+		}
+		// cpu := distributeEvenly(config.Infrastructure.CPU, absoluteTasks, config)
+		// mem := distributeEvenly(config.Infrastructure.Memory, absoluteTasks, config)
+		// fmt.Printf("CPU per group %d\n", cpu)
+		// fmt.Printf("Memory per group %d\n", mem)
+		availableCPU := config.Infrastructure.CPU * (1.0 - ((config.Infrastructure.SafetyMargin) / 100))
+		availableMemory := config.Infrastructure.Memory * (1.0 - ((config.Infrastructure.SafetyMargin) / 100))
+		weightlessTasks := absoluteTasks
+		// First assign resources to the groups that have a weight set in the config file
+		for _, group := range job.TaskGroups {
+			if group.Count == nil {
+				group.Count = new(int)
+				*group.Count = 1
+			}
+			for _, task := range group.Tasks {
+
+				sumOfWeights := 0
+				for _, groupConfig := range config.Groups {
+					sumOfWeights += groupConfig.Weight
+					if sumOfWeights > 100 {
+						return cli.NewExitError("Sum of weights greater than 100", 1)
+					}
+					if groupConfig.Name == *group.Name {
+
+						assignedMemory := config.Infrastructure.Memory * groupConfig.Weight / 100 / len(group.Tasks)
+						assignedCPU := config.Infrastructure.CPU * groupConfig.Weight / 100 / len(group.Tasks)
+						*task.Resources.MemoryMB = assignedMemory
+						*task.Resources.CPU = assignedCPU
+						availableCPU -= assignedCPU
+						availableMemory -= assignedMemory
+						weightlessTasks--
+					}
+				}
+			}
+		}
+		// Then evenly split up the rest
+		for _, group := range job.TaskGroups {
+			if group.Count == nil {
+				group.Count = new(int)
+				*group.Count = 1
+			}
+			found := false
+			for _, task := range group.Tasks {
+				for _, groupConfig := range config.Groups {
+					if groupConfig.Name == *group.Name {
+						found = true
+					}
+				}
+
+				if !found {
+					assignedMemory := availableMemory / weightlessTasks
+					assignedCPU := availableCPU / weightlessTasks
+					*task.Resources.MemoryMB = assignedMemory
+					*task.Resources.CPU = assignedCPU
+					// availableCPU -= assignedCPU
+					// availableMemory -= assignedMemory
+				}
+			}
+		}
+
+		for _, group := range job.TaskGroups {
+			c := color.New(color.FgBlack).Add(color.Underline)
+			c.Printf("%s", *group.Name)
+
+			c = color.New(color.FgBlack).Add(color.Italic)
+
+			if group.Count != nil {
+				c.Printf(" (%d)\n", *group.Count)
+			} else {
+				c.Printf("\n")
+			}
+			memoryOfGroup := 0
+			cpuOfGroup := 0
+			for _, task := range group.Tasks {
+				memoryOfGroup += *task.Resources.MemoryMB
+				cpuOfGroup += *task.Resources.CPU
+			}
+			drawUnitBar(memoryOfGroup, config.Infrastructure.Memory, "MB", "Memory")
+			drawUnitBar(cpuOfGroup, config.Infrastructure.CPU, "MHz", "CPU")
+			fmt.Println()
+
+			for _, task := range group.Tasks {
+				c := color.New(color.FgBlack).Add(color.Bold)
+				c.Println("\t" + task.Name)
+				c = color.New(color.FgBlack).Add(color.Italic)
+				// fmt.Println()
+				drawUnitBar(*task.Resources.MemoryMB, config.Infrastructure.Memory, "MB", "\tMemory")
+				drawUnitBar(*task.Resources.CPU, config.Infrastructure.CPU, "MHz", "\tCPU")
+			}
+		}
+
+		clientConfig := api.DefaultConfig()
+		clientConfig.Address = "http://localhost:4646"
+		// client, err := api.NewClient(clientConfig)
+		// client.Jobs().Plan(job, false, &api.WriteOptions{})
 
 		if err != nil {
 			log.Fatalf("%s", err)
 		}
-		fmt.Println(client.Agent().Region())
 		return nil
 	}
 
